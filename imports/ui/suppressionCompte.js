3fe60b603266ae395e129f1caa3745b56a5d7c6f
Pierre Morand <p.morand.vs@gmail.com>
2017-05-17 10:44:08 +0200

    Suppression de compte à travailler

--- imports/ui/suppressionCompte.js
+++ imports/ui/suppressionCompte.js
@@ -3,10 +3,30 @@ import './suppressionCompte.html';
 
 Template.suppressionCompte.events({
 	'click .deletion': function(event) {
-
+/*
 	UId = Meteor.userId()
 
 	Meteor.users.remove(UId);
-
+*/
+	Meteor.call('selfDelete');
 	}
+});
+
+Meteor.methods({
+	'selfdel'() {
+	Meteor.users.remove(this.userId);
+	}
+});
+
+Meteor.methods({
+  'selfDelete'() {
+    if (!Meteor.isServer) return;
+
+    try {
+      Meteor.users.remove(this.userId);
+    } catch (e) {
+      // handle this however you want
+      throw new Meteor.Error('self-delete', 'Failed to remove yourself');
+    }
+  },
 });
