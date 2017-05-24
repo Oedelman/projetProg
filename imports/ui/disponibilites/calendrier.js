5810ee24fbbdd79bad4981e1d7d6b8cedf01f4c1
fionapr <prfiona.elisabeth@gmail.com>
2017-05-24 09:03:57 +0200

    Echange jour et tranche

--- imports/ui/disponibilites/calendrier.js
+++ imports/ui/disponibilites/calendrier.js
@@ -7,20 +7,20 @@ import './tranche.js';
 import './calendrier.html';
 
 Template.calendrier.helpers({
-  tranches: [
-  	{nom: '6h-8h'},
-  	{nom: '10h-12h'},
-  	{nom: '12h-14h'},
-  	{nom: '14h-16h'},
-  ],
   nomsJours: [
-  	'Lundi',
-  	'Mardi',
-  	'Mercredi',
-  	'Jeudi',
-  	'Vendredi',
-  	'Samedi',
-  	'Dimanche'
+  	{nom: 'Lundi'},
+  	{nom: 'Mardi'},
+  	{nom: 'Mercredi'},
+  	{nom: 'Jeudi'},
+	{nom: 'Vendredi'},
+	{nom: 'Samedi'},
+	{nom: 'Dimanche'},
+  ],
+  tranches: [
+   '8h-12h',
+  '12h-16h',
+  '16h-20h',
+  '20h-00h',
   ],
   'disponibilites' (){
   	return Disponibilites.find({});
